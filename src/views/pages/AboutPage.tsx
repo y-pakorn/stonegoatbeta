1ab00e5b0d7bfec8661e9696cd295c719f9a0b99
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-10-22 16:38:38 +0700

    feat(about): add github info

--- src/views/pages/AboutPage.tsx
+++ src/views/pages/AboutPage.tsx
@@ -1,6 +1,7 @@
 import { Footer, Navbar, Section } from "@/components/common";
 import { AppHeader } from "@/components/common/AppHeader";
-import { Heading, Stack, Text } from "@chakra-ui/react";
+import { Heading, Icon, Link, Stack, Text } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa6";
 
 export const AboutPage = () => {
   return (
@@ -10,8 +11,14 @@ export const AboutPage = () => {
         <Navbar />
         <Stack py={[2, null, 8]}>
           <Heading>About</Heading>
-          <Text>Made with love by yoisha</Text>
-          <Text>Heh heh heh</Text>
+          <Text as="i" fontSize="lg">
+            This website is opensourced in{" "}
+            <Link href="https://github.com/y-pakorn/stonegoatbeta" isExternal>
+              Github <Icon as={FaGithub} />
+            </Link>
+            . Contribution and ideas suggestion are welcomed!
+          </Text>
+          <Text>Made with love by yoisha 🥰</Text>
         </Stack>
         <Footer />
       </Section>
