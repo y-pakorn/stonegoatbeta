a512e58bd80f6b7c864f9fb0fe76ac35964e7ae0
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-01-30 01:56:45 +0700

    feat: pull betas from db

--- src/pages/api/betas.ts
+++ src/pages/api/betas.ts
@@ -1,23 +1,30 @@
-import { axios } from "@/constants/axios";
 import { Beta } from "@/interfaces/beta";
+import { createClient } from "@supabase/supabase-js";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type ResponseData = {
-  betas: Beta[];
+  betas: Beta[] | null;
 };
 
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  if (!process.env.BETA_ENDPOINT) {
-    throw new Error("BETA_ENDPOINT is not defined");
+  if (!process.env.DB_URL) {
+    throw new Error("DB_URL is not defined");
   }
-  const betas: Beta[] = [];
-  const response = await axios.get(process.env.BETA_ENDPOINT);
+  if (!process.env.DB_API_KEY) {
+    throw new Error("DB_API_KEY is not defined");
+  }
 
-  betas.push(...response.data.data);
+  const supabase = createClient(process.env.DB_URL, process.env.DB_API_KEY);
+
+  const data = await supabase
+    .from("betas")
+    .select("*")
+    .order("timestamp", { ascending: false })
+    .limit(200);
 
   res.setHeader("Cache-Control", "public, s-maxage=600");
-  res.status(200).json({ betas });
+  res.status(200).json({ betas: data.data });
 }
