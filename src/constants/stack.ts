f7c66a621d8422d6c6f1f3a8009c0d00b4b4cff9
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-01-30 23:48:28 +0700

    feat: add tech stacks

---
+++ src/constants/stack.ts
@@ -0,0 +1,22 @@
+export const TECH_STACK = [
+  [
+    "Frontend",
+    ["React", "Next.js", "Chakra UI", "Zustand", "Font Awesome Icons"],
+  ],
+  [
+    "Backend",
+    [
+      "Vercel Serverless Functions",
+      "Supabase",
+      "Cloud Scheduler",
+      "PostgreSQL",
+    ],
+  ],
+  [
+    "Datasources",
+    [
+      "https://www.instagram.com/stonegoat_beta/",
+      "https://www.stonegoatclimb.com/",
+    ],
+  ],
+] as const;
--- src/views/pages/AboutPage.tsx
+++ src/views/pages/AboutPage.tsx
@@ -1,6 +1,16 @@
 import { Footer, Navbar, Section } from "@/components/common";
 import { AppHeader } from "@/components/common/AppHeader";
-import { Heading, Icon, Link, Stack, Text } from "@chakra-ui/react";
+import { TECH_STACK } from "@/constants/stack";
+import {
+  Divider,
+  Heading,
+  Icon,
+  Link,
+  ListItem,
+  Stack,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa6";
 
 export const AboutPage = () => {
@@ -18,7 +28,27 @@ export const AboutPage = () => {
             </Link>
             . Contribution and ideas suggestion are welcomed!
           </Text>
-          <Text>Made with love by yoisha 🥰</Text>
+          <Text>
+            Made with love by{" "}
+            <Link href="https://www.instagram.com/climbwithyoisha/" isExternal>
+              yoisha
+            </Link>{" "}
+            🥰
+          </Text>
+          <Divider my={4} />
+          <Heading fontSize="2xl">Tech Stack</Heading>
+          <UnorderedList pl={2}>
+            {TECH_STACK.map((t, i) => (
+              <ListItem key={i}>
+                <Text as="b">{t[0]}</Text>
+                <UnorderedList>
+                  {t[1].map((ts, j) => (
+                    <ListItem key={j}>{ts}</ListItem>
+                  ))}
+                </UnorderedList>
+              </ListItem>
+            ))}
+          </UnorderedList>
         </Stack>
         <Footer />
       </Section>
