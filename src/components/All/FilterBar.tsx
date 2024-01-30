7dc3cfa8a9027dee401edff6c57a9e4cce4b4799
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-01-30 15:15:05 +0700

    refactor: use filer bar

---
+++ src/components/All/FilterBar.tsx
@@ -0,0 +1,229 @@
+import { ZONES, getZoneByLabel } from "@/constants/zones";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  HStack,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItemOption,
+  MenuList,
+  MenuOptionGroup,
+  SimpleGrid,
+  Spacer,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import {
+  FaArrowDownWideShort,
+  FaArrowUpWideShort,
+  FaClockRotateLeft,
+  FaGraduationCap,
+  FaLocationDot,
+} from "react-icons/fa6";
+import { MiniGrade } from "./MiniGrade";
+import { GRADES_LABEL } from "@/constants/grades";
+import _ from "lodash";
+import { timeOptions } from "@/views/pages/AllPage";
+
+export const FilterBar = (filter: {
+  zones: string[];
+  grades: string[];
+  time: string;
+  timeSort: string;
+}) => {
+  const { query, replace } = useRouter();
+
+  return (
+    <HStack pos="sticky" top={0} bg="chakra-body-bg" py={[2, null, 4]}>
+      <Menu closeOnSelect={false}>
+        {({ isOpen }) => (
+          <>
+            <MenuButton
+              size={["sm", null, "md"]}
+              as={Button}
+              leftIcon={<Icon as={FaLocationDot} />}
+              rightIcon={
+                <Icon
+                  as={ChevronDownIcon}
+                  transform={isOpen ? "rotate(180deg)" : "rotate(0deg)"}
+                  transition="transform 0.2s ease-in-out"
+                />
+              }
+            >
+              {!filter.zones.length
+                ? "All"
+                : filter.zones.length > 1
+                ? "Multiple"
+                : getZoneByLabel(filter.zones[0])?.name}
+            </MenuButton>
+            <MenuList>
+              <MenuOptionGroup title="Walls/Zones" type="checkbox">
+                <SimpleGrid columns={1}>
+                  {ZONES.map((z) => (
+                    <MenuItemOption
+                      key={z.label}
+                      value={z.label}
+                      isChecked={filter.zones.includes(z.label)}
+                      onClick={() => {
+                        const zones = filter.zones.includes(z.label)
+                          ? filter.zones.filter((g) => g !== z.label)
+                          : [...filter.zones, z.label];
+                        replace({
+                          query: {
+                            ...query,
+                            zones: zones.join(","),
+                          },
+                        });
+                      }}
+                    >
+                      {z.name}
+                    </MenuItemOption>
+                  ))}
+                </SimpleGrid>
+              </MenuOptionGroup>
+            </MenuList>
+          </>
+        )}
+      </Menu>
+      <Menu closeOnSelect={false}>
+        {({ isOpen }) => (
+          <>
+            <MenuButton
+              size={["sm", null, "md"]}
+              as={Button}
+              leftIcon={<Icon as={FaGraduationCap} />}
+              rightIcon={
+                <Icon
+                  as={ChevronDownIcon}
+                  transform={isOpen ? "rotate(180deg)" : "rotate(0deg)"}
+                  transition="transform 0.2s ease-in-out"
+                />
+              }
+            >
+              {!filter.grades.length ? (
+                "All"
+              ) : filter.grades.length > 1 ? (
+                "Multiple"
+              ) : (
+                <MiniGrade grade={filter.grades[0]} />
+              )}
+            </MenuButton>
+            <MenuList>
+              <MenuOptionGroup title="Grades" type="checkbox">
+                <SimpleGrid columns={2}>
+                  {GRADES_LABEL.map((z) => (
+                    <MenuItemOption
+                      key={z}
+                      value={z}
+                      onClick={() => {
+                        const grades = filter.grades.includes(z)
+                          ? filter.grades.filter((g) => g !== z)
+                          : [...filter.grades, z];
+                        replace({
+                          query: {
+                            ...query,
+                            grades: grades.join(","),
+                          },
+                        });
+                      }}
+                      isChecked={filter.grades.includes(z)}
+                    >
+                      <MiniGrade grade={z} />
+                    </MenuItemOption>
+                  ))}
+                </SimpleGrid>
+              </MenuOptionGroup>
+            </MenuList>
+          </>
+        )}
+      </Menu>
+      <Spacer />
+      <Menu closeOnSelect={false}>
+        {({ isOpen }) => (
+          <>
+            <MenuButton
+              size={["sm", null, "md"]}
+              as={Button}
+              leftIcon={<Icon as={FaClockRotateLeft} />}
+              rightIcon={
+                <HStack>
+                  <Icon
+                    as={
+                      filter.timeSort === "desc"
+                        ? FaArrowDownWideShort
+                        : FaArrowUpWideShort
+                    }
+                  />
+                  <Icon
+                    as={ChevronDownIcon}
+                    transform={isOpen ? "rotate(180deg)" : "rotate(0deg)"}
+                    transition="transform 0.2s ease-in-out"
+                  />
+                </HStack>
+              }
+            >
+              {_.startCase(filter.time).slice(0, 3).replace(" ", "")}
+            </MenuButton>
+            <MenuList>
+              <MenuOptionGroup title="Time Range" type="radio">
+                <SimpleGrid columns={1}>
+                  {Object.keys(timeOptions).map((z) => (
+                    <MenuItemOption
+                      key={z}
+                      value={z}
+                      isChecked={filter.time === z}
+                      onClick={() =>
+                        replace({
+                          query: {
+                            ...query,
+                            time: z,
+                          },
+                        })
+                      }
+                    >
+                      {_.startCase(z)}
+                    </MenuItemOption>
+                  ))}
+                </SimpleGrid>
+              </MenuOptionGroup>
+              <MenuDivider />
+              <MenuOptionGroup title="Time Order" type="radio">
+                <SimpleGrid columns={1}>
+                  <MenuItemOption
+                    value="desc"
+                    isChecked={filter.timeSort === "desc"}
+                    onClick={() =>
+                      replace({
+                        query: {
+                          ...query,
+                          timeSort: "desc",
+                        },
+                      })
+                    }
+                  >
+                    Descending
+                  </MenuItemOption>
+                  <MenuItemOption
+                    value="asc"
+                    isChecked={filter.timeSort !== "desc"}
+                    onClick={() =>
+                      replace({
+                        query: {
+                          ...query,
+                          timeSort: "asc",
+                        },
+                      })
+                    }
+                  >
+                    Ascending
+                  </MenuItemOption>
+                </SimpleGrid>
+              </MenuOptionGroup>
+            </MenuList>
+          </>
+        )}
+      </Menu>
+    </HStack>
+  );
+};
--- src/views/pages/AllPage.tsx
+++ src/views/pages/AllPage.tsx
@@ -1,42 +1,25 @@
+import { FilterBar } from "@/components/All/FilterBar";
 import { MiniGrade } from "@/components/All/MiniGrade";
 import { BetaCard } from "@/components/Card/BetaCard";
 import { PlayerModal } from "@/components/Modal/PlayerModal";
 import { Footer, Navbar, Section } from "@/components/common";
 import { AppHeader } from "@/components/common/AppHeader";
-import { GRADES_LABEL } from "@/constants/grades";
-import { ZONES, getZoneByLabel } from "@/constants/zones";
+import { getZoneByLabel } from "@/constants/zones";
 import { BetaInfo } from "@/interfaces/beta";
 import { useBetas } from "@/stores/useBetas";
-import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
-  Button,
   Center,
   CircularProgress,
   HStack,
-  Icon,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItemOption,
-  MenuList,
-  MenuOptionGroup,
   SimpleGrid,
-  Spacer,
   Stack,
   Text,
 } from "@chakra-ui/react";
 import _ from "lodash";
 import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
-import {
-  FaArrowDownWideShort,
-  FaArrowUpWideShort,
-  FaClockRotateLeft,
-  FaGraduationCap,
-  FaLocationDot,
-} from "react-icons/fa6";
 
-const timeOptions = {
+export const timeOptions = {
   "1Day": 1,
   "1Week": 7,
   "1Month": 30,
@@ -54,7 +37,7 @@ export const AllPage = () => {
       zones: query.zones ? query.zones.toString().split(",") || [] : [],
       grades: query.grades ? query.grades.toString().split(",") || [] : [],
       time: (query.time || defaultTime) as keyof typeof timeOptions,
-      timeSort: query.timeSort || defaultTimeSort,
+      timeSort: (query.timeSort || defaultTimeSort) as string,
     } as const;
   }, [query]);
 
@@ -111,196 +94,7 @@ export const AllPage = () => {
           beta={selectedBeta}
         />
         <Stack py={[2, null, 8]}>
-          <HStack pos="sticky" top={0} bg="chakra-body-bg" py={[2, null, 4]}>
-            <Menu closeOnSelect={false}>
-              {({ isOpen }) => (
-                <>
-                  <MenuButton
-                    size={["sm", null, "md"]}
-                    as={Button}
-                    leftIcon={<Icon as={FaLocationDot} />}
-                    rightIcon={
-                      <Icon
-                        as={ChevronDownIcon}
-                        transform={isOpen ? "rotate(180deg)" : "rotate(0deg)"}
-                        transition="transform 0.2s ease-in-out"
-                      />
-                    }
-                  >
-                    {!filter.zones.length
-                      ? "All"
-                      : filter.zones.length > 1
-                      ? "Multiple"
-                      : getZoneByLabel(filter.zones[0])?.name}
-                  </MenuButton>
-                  <MenuList>
-                    <MenuOptionGroup title="Walls/Zones" type="checkbox">
-                      <SimpleGrid columns={1}>
-                        {ZONES.map((z) => (
-                          <MenuItemOption
-                            key={z.label}
-                            value={z.label}
-                            isChecked={filter.zones.includes(z.label)}
-                            onClick={() => {
-                              const zones = filter.zones.includes(z.label)
-                                ? filter.zones.filter((g) => g !== z.label)
-                                : [...filter.zones, z.label];
-                              replace({
-                                query: {
-                                  ...query,
-                                  zones: zones.join(","),
-                                },
-                              });
-                            }}
-                          >
-                            {z.name}
-                          </MenuItemOption>
-                        ))}
-                      </SimpleGrid>
-                    </MenuOptionGroup>
-                  </MenuList>
-                </>
-              )}
-            </Menu>
-            <Menu closeOnSelect={false}>
-              {({ isOpen }) => (
-                <>
-                  <MenuButton
-                    size={["sm", null, "md"]}
-                    as={Button}
-                    leftIcon={<Icon as={FaGraduationCap} />}
-                    rightIcon={
-                      <Icon
-                        as={ChevronDownIcon}
-                        transform={isOpen ? "rotate(180deg)" : "rotate(0deg)"}
-                        transition="transform 0.2s ease-in-out"
-                      />
-                    }
-                  >
-                    {!filter.grades.length ? (
-                      "All"
-                    ) : filter.grades.length > 1 ? (
-                      "Multiple"
-                    ) : (
-                      <MiniGrade grade={filter.grades[0]} />
-                    )}
-                  </MenuButton>
-                  <MenuList>
-                    <MenuOptionGroup title="Grades" type="checkbox">
-                      <SimpleGrid columns={2}>
-                        {GRADES_LABEL.map((z) => (
-                          <MenuItemOption
-                            key={z}
-                            value={z}
-                            onClick={() => {
-                              const grades = filter.grades.includes(z)
-                                ? filter.grades.filter((g) => g !== z)
-                                : [...filter.grades, z];
-                              replace({
-                                query: {
-                                  ...query,
-                                  grades: grades.join(","),
-                                },
-                              });
-                            }}
-                            isChecked={filter.grades.includes(z)}
-                          >
-                            <MiniGrade grade={z} />
-                          </MenuItemOption>
-                        ))}
-                      </SimpleGrid>
-                    </MenuOptionGroup>
-                  </MenuList>
-                </>
-              )}
-            </Menu>
-            <Spacer />
-            <Menu closeOnSelect={false}>
-              {({ isOpen }) => (
-                <>
-                  <MenuButton
-                    size={["sm", null, "md"]}
-                    as={Button}
-                    leftIcon={<Icon as={FaClockRotateLeft} />}
-                    rightIcon={
-                      <HStack>
-                        <Icon
-                          as={
-                            filter.timeSort === "desc"
-                              ? FaArrowDownWideShort
-                              : FaArrowUpWideShort
-                          }
-                        />
-                        <Icon
-                          as={ChevronDownIcon}
-                          transform={isOpen ? "rotate(180deg)" : "rotate(0deg)"}
-                          transition="transform 0.2s ease-in-out"
-                        />
-                      </HStack>
-                    }
-                  >
-                    {_.startCase(filter.time).slice(0, 3).replace(" ", "")}
-                  </MenuButton>
-                  <MenuList>
-                    <MenuOptionGroup title="Time Range" type="radio">
-                      <SimpleGrid columns={1}>
-                        {Object.keys(timeOptions).map((z) => (
-                          <MenuItemOption
-                            key={z}
-                            value={z}
-                            isChecked={filter.time === z}
-                            onClick={() =>
-                              replace({
-                                query: {
-                                  ...query,
-                                  time: z,
-                                },
-                              })
-                            }
-                          >
-                            {_.startCase(z)}
-                          </MenuItemOption>
-                        ))}
-                      </SimpleGrid>
-                    </MenuOptionGroup>
-                    <MenuDivider />
-                    <MenuOptionGroup title="Time Order" type="radio">
-                      <SimpleGrid columns={1}>
-                        <MenuItemOption
-                          value="desc"
-                          isChecked={filter.timeSort === "desc"}
-                          onClick={() =>
-                            replace({
-                              query: {
-                                ...query,
-                                timeSort: "desc",
-                              },
-                            })
-                          }
-                        >
-                          Descending
-                        </MenuItemOption>
-                        <MenuItemOption
-                          value="asc"
-                          isChecked={filter.timeSort !== "desc"}
-                          onClick={() =>
-                            replace({
-                              query: {
-                                ...query,
-                                timeSort: "asc",
-                              },
-                            })
-                          }
-                        >
-                          Ascending
-                        </MenuItemOption>
-                      </SimpleGrid>
-                    </MenuOptionGroup>
-                  </MenuList>
-                </>
-              )}
-            </Menu>
-          </HStack>
+          <FilterBar {...filter} />
           {isLoading ? (
             <Center h="50dvh">
               <CircularProgress isIndeterminate color="primary.500" />
